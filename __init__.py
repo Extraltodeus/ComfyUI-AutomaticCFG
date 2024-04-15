cc24dbcc02dc7856d793ab764a19705c2ae7239b
Extraltodeus <extraltodeus@gmail.com>
2024-04-15 08:12:25 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,5 +3,6 @@ from .nodes import *
 NODE_CLASS_MAPPINGS = {
     "Automatic CFG": simpleDynamicCFG,
     "Automatic CFG - Negative": simpleDynamicCFGlerpUncond,
+    "Automatic CFG - No uncond": simpleDynamicCFGNoUncond,
     "Automatic CFG - Advanced settings": advancedDynamicCFG,
 }
