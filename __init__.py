d25d0c69f4badd40dde9057aabb5fc2e424e3738
Extraltodeus <extraltodeus@gmail.com>
2024-04-17 18:22:42 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ from .nodes_sag_custom import *
 NODE_CLASS_MAPPINGS = {
     "Automatic CFG": simpleDynamicCFG,
     "Automatic CFG - Negative": simpleDynamicCFGlerpUncond,
+    "Automatic CFG - Fastest": simpleDynamicCFGHighSpeed,
     "Automatic CFG - Advanced": advancedDynamicCFG,
     "Automatic CFG - Post rescale only": postCFGrescaleOnly,
     "SAG delayed activation": SelfAttentionGuidanceCustom,
