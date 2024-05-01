c4d4d879bf448d3964c8914db2bb18bd1f43e2b8
Extraltodeus <extraltodeus@gmail.com>
2024-05-01 23:23:35 +0200

    Add files via upload

--- nodes.py
+++ nodes.py
@@ -508,7 +508,7 @@ class simpleDynamicCFGwarpDrive:
 
     def patch(self, model, uncond_sigma_start, uncond_sigma_end, fake_uncond_sigma_end):
         advcfg = advancedDynamicCFG()
-        print(f"{Fore.CYAN}WARP DRIVE MODE ENGAGED!{Style.RESET_ALL}\nSettings suggestions:\n"
+        print(f"                                                {Fore.CYAN}WARP DRIVE MODE ENGAGED!{Style.RESET_ALL}\n    Settings suggestions:\n"
             f"              {Fore.GREEN}1/1/1:   {Fore.YELLOW}Maaaxxxiiimum speeeeeed.{Style.RESET_ALL} {Fore.RED}Uncond disabled.{Style.RESET_ALL} {Fore.MAGENTA}Fasten your seatbelt!{Style.RESET_ALL}\n"
             f"              {Fore.GREEN}3/1/1:   {Fore.YELLOW}Risky space-time continuum distortion.{Style.RESET_ALL} {Fore.MAGENTA}Awesome for prompts with a clear subject!{Style.RESET_ALL}\n"
             f"              {Fore.GREEN}5.5/1/1: {Fore.YELLOW}Frameshift Drive Autopilot: {Fore.GREEN}Engaged.{Style.RESET_ALL} {Fore.MAGENTA}Should work with anything but do it better and faster!{Style.RESET_ALL}")
